e2656b10fa80cc6f08dcf3ea315ad6764e0540cf
Jinjiarui <jinjiarui97@gmail.com>
2023-06-18 19:05:37 +0000

    evaluator

---
+++ system/evaluator.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+import random
+
+from transformers import is_torch_available
+
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    if is_torch_available():
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+def evaluate_offline_trainer():
+    pass
+
+def evaluate_online_checker():
+    pass
---
+++ system/run.py
@@ -0,0 +1,5 @@
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
