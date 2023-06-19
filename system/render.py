4e167d70e69884f79ff8a8172686e4e13a940618
Jinjiarui <jinjiarui97@gmail.com>
2023-06-19 13:19:48 +0000

    minor

--- system/render.py
+++ system/render.py
@@ -29,6 +29,23 @@ class BaseRender():
         raise NotImplementedError
 
 
+class RuleRender(BaseRender):
+    def __init__(self, model_name: str = "gpt-3.5-turbo"):
+        self.model_name = model_name    
+        
+    def response2ids(self, response: str, item_ids: List[int], attribute_ids: Dict[int, List[int]]):
+        raise NotImplementedError
+    
+    def ids2response(self, query_type: str, query_id, response_ids) -> str:
+        raise NotImplementedError
+
+    def ids2query(self, query_type: str, query_id) -> str:
+        raise NotImplementedError
+    
+    def query2ids(self, query: str, item_ids: List[int], attribute_ids: Dict[int, List[int]]):
+        raise NotImplementedError
+    
+
 class LMRender(BaseRender):
     def __init__(self, model_name: str = "gpt-3.5-turbo"):
         self.model_name = model_name    
